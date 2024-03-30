48e42ea70bd563c3e558cd2557ad1d9c4dda68e3
akanksha <akankshakushwah1501@gmail.com>
2024-03-30 21:47:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMenu() {
+  const menu = document.querySelector("#hamburger-nav .nav-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+
+
+
+// Holding a B.Tech degree in Computer Science, India. I possess a strong foundation in
+//             programming languages. Currently, I am engaged in intensive study of the MERN stack to broaden my skillset
+//             and become a full-stack developer. My passion lies in creating user interfaces that are not only visually
+//             appealing but also
+//             highly responsive and user-friendly. 
